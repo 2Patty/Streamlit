d5d5bccdad644aa8377352b7fbed81c8e04c5836
2Patty <gathurawanjiku02@gmail.com>
2024-11-15 18:28:53 +0300

    changes5

--- auth.py
+++ auth.py
@@ -30,7 +30,7 @@ def show_authenticated_content():
     #st.sidebar.button("Home", on_click=home_page)
 
     # Add a logout button
-    if st.sidebar.button("Logout"):
+    if st.sidebar.button("Logout", key ="logout_button"):
         st.session_state.authenticated = False
         st.success("Logged out!")
 
